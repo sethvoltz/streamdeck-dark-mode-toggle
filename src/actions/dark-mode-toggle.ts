de9c6889a7f4cb59ec195fa443bc03636c897f1e
Seth Voltz <seth@designgods.net>
2025-07-16 13:56:00 -0700

    feat: timer based checking of current light/dark mode to update button image

--- src/actions/dark-mode-toggle.ts
+++ src/actions/dark-mode-toggle.ts
@@ -1,11 +1,15 @@
-import { action, KeyAction, KeyDownEvent, SingletonAction, WillAppearEvent } from "@elgato/streamdeck";
+import { action, KeyAction, KeyDownEvent, SingletonAction, WillAppearEvent, WillDisappearEvent } from "@elgato/streamdeck";
 import darkMode from 'dark-mode';
+
+const UPDATE_INTERVAL = 10 * 1000; // 10 seconds
 
 /**
  * An example action class that displays a count that increments by one each time the button is pressed.
  */
 @action({ UUID: "com.voltzmakes.dark-mode-toggle.toggle" })
 export class DarkModeToggle extends SingletonAction<ToggleSettings> {
+	private timer: NodeJS.Timeout | undefined;
+
 	/**
 	 * The {@link SingletonAction.onWillAppear} event is useful for setting the visual representation of an action when it becomes visible. This could be due to the Stream Deck first
 	 * starting up, or the user navigating between pages / folders etc.. There is also an inverse of this event in the form of {@link streamDeck.client.onWillDisappear}. In this example,
@@ -14,6 +18,27 @@ export class DarkModeToggle extends SingletonAction<ToggleSettings> {
 	override async onWillAppear(ev: WillAppearEvent<ToggleSettings>): Promise<void> {
 		if (!ev.action.isKey()) return;
 		this.updateButtonState(ev.action);
+
+		if (!this.timer) {
+			this.timer = setInterval(() => {
+				for (const action of this.actions) {
+					// Verify that the action is a key so we can call setRandomCat.
+					if (action.isKey()) {
+						this.updateButtonState(action);
+					}
+				}
+			}, UPDATE_INTERVAL);
+		}
+	}
+
+	/**
+	 * Removes the action from the auto-update list (as it is no longer visible) and clears the timer if no more actions are auto-updating.
+	 */
+	override async onWillDisappear(ev: WillDisappearEvent<ToggleSettings>): Promise<void> {
+		if (this.actions.next().done) {
+			clearInterval(this.timer);
+			this.timer = undefined;
+		}
 	}
 
 	/**
